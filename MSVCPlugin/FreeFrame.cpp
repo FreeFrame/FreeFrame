97e1f094245577b247afe99e91f745292364eb10
rayvaughn <>
2002-11-16 15:43:41 +0000

    First go at FreeFrame plugin in MSVC 6 SP5

---
+++ MSVCPlugin/FreeFrame.cpp
@@ -0,0 +1,167 @@
+// FreeFrame.cpp : Defines the entry point for the DLL application.
+//
+
+
+#include "stdafx.h"
+#include "FreeFrame.h"
+#include <stdio.h>
+
+#define NUM_PARAMS 3
+
+//////////////////////////////////////////////////////////////
+// DLL Entry point
+//
+// notes: we may want to capture hModule as the instance of the host...
+
+BOOL APIENTRY DllMain( HANDLE hModule, 
+                       DWORD  ul_reason_for_call, 
+                       LPVOID lpReserved
+					 )
+{
+    return TRUE;
+}
+
+///////////////////////////////////////////////////////////////
+// main - The one and only exposed function
+// parameters: 
+//	functionCode - tells the plugin which function is being called
+//  pParam - 32-bit pointer to parameter
+
+__declspec(dllexport) LPVOID __stdcall plugMain(DWORD functionCode, LPVOID pParam)
+ {
+	
+	switch(functionCode) {
+
+	case FF_GETINFO:
+		return (LPVOID) getInfo();
+	
+	case FF_INITIALISE:
+		return (LPVOID) initialise();
+	
+	case FF_DEINITIALISE:
+		return (LPVOID) deinitialise();
+	
+	case FF_GETNUMPARAMETERS:
+		return (LPVOID) getNumParameters();
+
+	case FF_GETPARAMETERNAME:
+		return (LPVOID) getParameterName( (DWORD) pParam );
+	
+	case FF_GETPARAMETERDEFAULT:
+		return (LPVOID)(DWORD) getParameterDefault( (DWORD) pParam );
+	
+	case FF_GETPARAMETERDISPLAY:
+		return  (LPVOID) getParameterDisplay( (DWORD) pParam );
+	
+	case FF_SETPARAMETER:
+		return (LPVOID) setParameter( (DWORD) pParam, *((float*)(pParam)+1) );
+	
+	case FF_PROCESSFRAME:
+		return (LPVOID) processFrame(pParam);
+
+	case FF_GETPARAMETER:
+		return (LPVOID)(DWORD) getParameter((DWORD) pParam);
+
+	case FF_GETPLUGINCAPS:
+		return (LPVOID) getPluginCaps( (DWORD) pParam);
+	
+	default:
+		return (LPVOID) FF_FAIL;
+	}
+}
+
+/////////////////////////////////////////////////////
+//
+// Plugin Globals
+//
+
+PlugInfoStruct info;
+ParameterStruct parameters[NUM_PARAMS];
+
+PlugInfoStruct* getInfo() 
+{
+	info.APIMajorVersion = 0;
+	info.APIMinorVersion = 1;
+	char ID[5] = "MPB1";
+	char name[17] = "MSVC_Plugin_Base";
+	
+	memcpy(info.uniqueID, ID, 4);
+	memcpy(info.pluginName, name, 16);
+	info.pluginType = FF_EFFECT;
+
+	return &info;
+}
+
+// set up the parameters
+DWORD initialise()
+{
+	parameters[0].defaultValue = 0.0f;
+	parameters[1].defaultValue = 0.5f;
+	parameters[2].defaultValue = 1.0f;
+	parameters[0].value = 0.0f;
+	parameters[1].value = 0.5f;
+	parameters[2].value = 1.0f;
+	char tempName1[17] = "MSVC_plug_param1";
+	char tempName2[17] = "MSVC_plug_param2";
+	char tempName3[17] = "MSVC_plug_param3";
+	memcpy(parameters[0].name, tempName1, 16);
+	memcpy(parameters[1].name, tempName2, 16);
+	memcpy(parameters[2].name, tempName3, 16);
+	return 23;
+}
+
+// deallocate memory, tidy up
+DWORD deinitialise()
+{
+	return 23;
+}
+
+
+// returns number of parameters
+DWORD getNumParameters()
+{
+	return NUM_PARAMS;  // 1 param for now
+}
+
+// returns string with the name 0f parameter n
+char* getParameterName(DWORD index)
+{
+	return parameters[index].name;
+}
+
+// returns value  of parameter n
+float getParameterDefault(DWORD index)
+{
+	return parameters[index].defaultValue;
+}
+
+// returns string containing value of parameter to display
+char* getParameterDisplay(DWORD index)
+{
+
+	sprintf(parameters[index].displayValue, "%f",parameters[index].value);
+	return parameters[index].displayValue;
+}
+
+// returns success/fail - sets the value of parameter n in response to user
+DWORD setParameter(DWORD index, float value)
+{
+	parameters[index].value = value;
+	return 0;
+}
+
+float getParameter(DWORD index)
+{
+	return parameters[index].value;
+}
+
+// process frame of video, returns success/fail
+DWORD processFrame(LPVOID pFrame)
+{
+	return 999;
+}
+
+DWORD getPluginCaps(DWORD cap)
+{
+	return 0;
+}
---
+++ MSVCPlugin/FreeFrame.h
@@ -0,0 +1,70 @@
+//////////////////////////////////////////////////////////////
+// FreeFrame.h
+
+#ifndef __FREEFRAME_H__
+#define __FREEFRAME_H__
+
+
+//////////////////////////////////////////////////////////////
+// #defines
+
+#define FF_EFFECT				0
+#define FF_FAIL					0xFFFFFFFF
+
+#define FF_GETINFO				0
+#define FF_INITIALISE			1
+#define FF_DEINITIALISE			2
+#define FF_PROCESSFRAME			3
+#define FF_GETNUMPARAMETERS		4
+#define FF_GETPARAMETERNAME		5
+#define FF_GETPARAMETERDEFAULT	6
+#define FF_GETPARAMETERDISPLAY	7
+#define FF_SETPARAMETER			8
+#define FF_GETPARAMETER			9
+#define FF_GETPLUGINCAPS		10
+
+
+struct PlugInfoStruct {
+	DWORD	APIMajorVersion;
+	DWORD	APIMinorVersion;
+	BYTE	uniqueID[4];			// 4 chars uniqueID - not null terminated
+	BYTE	pluginName[16];			// 16 chars plugin friendly name - not null terminated
+	DWORD	pluginType;				// Effect or source
+};
+
+struct ParameterStruct {
+	float value;
+	float defaultValue;
+	char displayValue[16];
+	char name[16];
+};
+
+
+//////////////////////////////////////////////////////////////
+// Function prototypes
+
+#define windoze  // we'll sort this later!
+
+#ifdef windoze
+BOOL APIENTRY DllMain( HANDLE hModule, 
+                       DWORD  ul_reason_for_call, 
+                       LPVOID lpReserved );
+
+__declspec(dllexport) LPVOID __stdcall plugMain(DWORD functionCode, LPVOID pParam);
+#endif
+// linux goes here!
+
+
+PlugInfoStruct*	getInfo();							// get information about plugin
+DWORD	initialise();								// allocate memory, set defaults
+DWORD	deinitialise();								// deallocate memory, tidy up
+DWORD	getNumParameters();							// returns number of parameters
+char*	getParameterName(DWORD index);				// returns string with the name if parameter n
+float	getParameterDefault(DWORD index);			// returns value  of parameter n
+char*	getParameterDisplay(DWORD index);			// returns string containing value of parameter to display
+DWORD	setParameter(DWORD index, float value);		// returns success/fail - sets the value of parameter n in response to user
+float	getParameter(DWORD index);					// returns value of parameter n
+DWORD	processFrame(LPVOID pFrame);				// process frame of video, returns success/fail
+DWORD	getPluginCaps(DWORD cap);
+
+#endif
