e21d7752bd925b931848ce61d0dc4d7b779880e8
rayvaughn <>
2002-11-18 12:57:38 +0000

    this plugin now implements brightness (er.. darkness!!) in delphi host version 1024 added videoPixel24bit struct added setparameter struct added a load of comments to make things clearer

--- MSVCPlugin/FreeFrame.cpp
+++ MSVCPlugin/FreeFrame.cpp
@@ -1,14 +1,63 @@
-// FreeFrame.cpp : Defines the entry point for the DLL application.
+///////////////////////////////////////////////////////////////////////////////////
+// FreeFrame.cpp
 //
+// FreeFrame Open Video Plugin Prototype
+// ANSI C Version
 
+// www.freeframe.org
+// marcus@freeframe.org
+
+/*
+
+Copyright (c) 2002, Marcus Clements www.freeframe.org
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in
+     the documentation and/or other materials provided with the
+     distribution.
+   * Neither the name of FreeFrame nor the names of its
+     contributors may be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+// includes 
+//
 
 #include "FreeFrame.h"
 #include <stdio.h>
 #include <string.h>
 
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+// implementation specific definitions
+//
+
 #define NUM_PARAMS 3
 
-//////////////////////////////////////////////////////////////
+struct ParameterStruct {
+	float value;
+	float defaultValue;
+	char displayValue[16];
+	char name[16];
+};
+
+struct VideoPixel24bit {
+	BYTE blue;
+	BYTE green;
+	BYTE red;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////
 // Windows DLL Entry point
 //
 // notes: we may want to capture hModule as the instance of the host...
@@ -23,11 +72,20 @@ BOOL APIENTRY DllMain( HANDLE hModule,
 }
 #endif
 
-///////////////////////////////////////////////////////////////
-// main - The one and only exposed function
+///////////////////////////////////////////////////////////////////////////////////////
+// plugMain - The one and only exposed function
 // parameters: 
 //	functionCode - tells the plugin which function is being called
 //  pParam - 32-bit parameter or 32-bit pointer to parameter structure
+//
+// PLUGIN DEVELOPERS:  you shouldn't need to change this function
+//
+// All parameters are cast as 32-bit untyped pointers and cast to appropriate
+// types here
+// 
+// All return values are cast to 32-bit untyped pointers here before return to 
+// the host
+//
 
 #ifdef WIN32
  __declspec(dllexport) LPVOID __stdcall plugMain(DWORD functionCode, LPVOID pParam) 
@@ -41,10 +99,10 @@ BOOL APIENTRY DllMain( HANDLE hModule,
 		return (LPVOID) getInfo();
 	
 	case FF_INITIALISE:
-		return (LPVOID) initialise();
+		return (LPVOID) initialise( (VideoInfoStruct*) pParam);
 	
 	case FF_DEINITIALISE:
-		return (LPVOID) deinitialise();
+		return (LPVOID) deInitialise();
 	
 	case FF_GETNUMPARAMETERS:
 		return (LPVOID) getNumParameters();
@@ -58,8 +116,10 @@ BOOL APIENTRY DllMain( HANDLE hModule,
 	case FF_GETPARAMETERDISPLAY:
 		return  (LPVOID) getParameterDisplay( (DWORD) pParam );
 	
+	// parameters are passed in here as a packed struct of two DWORDS:
+	// index and value
 	case FF_SETPARAMETER:
-		return (LPVOID) setParameter( (DWORD) pParam, *((float*)(pParam)+1) );
+		return (LPVOID) setParameter( (SetParameterStruct*) pParam );
 	
 	case FF_PROCESSFRAME:
 		return (LPVOID) processFrame(pParam);
@@ -80,95 +140,278 @@ BOOL APIENTRY DllMain( HANDLE hModule,
 //
 // Plugin Globals
 //
+// these are defined globally in this sample plugin for simplicities sake
+// Plugin developers should allocate memory from the heap for this stuff
+//
 
-PlugInfoStruct info;
+PlugInfoStruct plugInfo;
+VideoInfoStruct videoInfo;
 ParameterStruct parameters[NUM_PARAMS];
+
+
+/////////////////////////////////////////////////////
+//
+// Sample Function implementation
+//
+
+///////////////////////////////////////////////////////////////////////////////////////
+// getInfo
+//
+// gets information about the plugin - version, unique id, short name and type 
+// This function should be identical in all future versions of the FreeFrame API
+//  
+// return values (32-bit pointer to PlugInfoStruct)
+// FF_FAIL on error
+// 32-bit pointer to PlugInfoStruct
+//
+// HOST: call this function first to get version information
+//       the version defines the other fucntion codes that are supported
+//       supported function codes are listed in the documentation www.freeframe.org
 
 PlugInfoStruct* getInfo() 
 {
-	info.APIMajorVersion = 0;
-	info.APIMinorVersion = 1024; // lets keep this in sync with the delphi host for now
-	char ID[5] = "MPB1";
+	plugInfo.APIMajorVersion = 0;
+	plugInfo.APIMinorVersion = 1024; // lets keep this in sync with the delphi host for now
+	char ID[5] = "MPB1";		 // this *must* be unique to your plugin 
+								 // see www.freeframe.org for a list of ID's already taken
 	char name[17] = "MSVC_Plugin_Base";
 	
-	memcpy(info.uniqueID, ID, 4);
-	memcpy(info.pluginName, name, 16);
-	info.pluginType = FF_EFFECT;
+	memcpy(plugInfo.uniqueID, ID, 4);
+	memcpy(plugInfo.pluginName, name, 16);
+	plugInfo.pluginType = FF_EFFECT;
 
-	return &info;
+	return &plugInfo;
 }
 
-// set up the parameters
-DWORD initialise()
+///////////////////////////////////////////////////////////////////////////////////////
+// initialise
+//
+// Prepare the Plug-in for processing.  
+// Set default values, allocate memory
+// When the plug-in returns from this function it must be ready to proces a frame
+//
+// return values (DWORD)
+// FF_SUCCESS - success
+// non-zero - fail (error values to be defined)
+//
+// HOST: This function *must* return before a call to processFrame
+
+DWORD initialise(VideoInfoStruct* pVideoInfo)
 {
-	parameters[0].defaultValue = 0.0f;
-	parameters[1].defaultValue = 0.5f;
+	// make a copy of the VideoInfoStruct
+	videoInfo.frameWidth = pVideoInfo->frameWidth;
+	videoInfo.frameHeight = pVideoInfo->frameHeight;
+	videoInfo.bitDepth = pVideoInfo->bitDepth;
+
+	// this shouldn't happen if the host is checking the capabilities properly
+	if (videoInfo.bitDepth != 1)
+	{
+		return FF_FAIL;
+	}
+
+	// populate the parameters structs
+	parameters[0].defaultValue = 0.5f;
+	parameters[1].defaultValue = 0.0f;
 	parameters[2].defaultValue = 1.0f;
-	parameters[0].value = 0.0f;
-	parameters[1].value = 0.5f;
+	parameters[0].value = 0.5f;
+	parameters[1].value = 0.0f;
 	parameters[2].value = 1.0f;
-	char tempName1[17] = "MSVC_plug_param1";
-	char tempName2[17] = "MSVC_plug_param2";
-	char tempName3[17] = "MSVC_plug_param3";
+	char tempName1[17] = "brightness";
+	char tempName2[17] = "unused____param2";
+	char tempName3[17] = "unused____param3";
 	memcpy(parameters[0].name, tempName1, 16);
 	memcpy(parameters[1].name, tempName2, 16);
 	memcpy(parameters[2].name, tempName3, 16);
-	return 23;
+
+	return FF_SUCCESS;
 }
 
-// deallocate memory, tidy up
-DWORD deinitialise()
+///////////////////////////////////////////////////////////////////////////////////////
+// deinitialise
+//
+// Tidy up   
+// Deallocate memory
+//
+// return values (DWORD)
+// FF_SUCCESS - success
+// non-zero - fail (error values to be defined)
+//
+// HOST: This *must* be the last function called on the plugin
+
+DWORD deInitialise()
 {
-	return 23;
+	return FF_SUCCESS;
 }
 
+///////////////////////////////////////////////////////////////////////////////////////
+// getNumParameters 
+//
+// returns number of parameters in plugin
+// 
+// return values (DWORD)
+// number of parameters
+// FF_FAIL on error
+//
 
-// returns number of parameters
 DWORD getNumParameters()
 {
 	return NUM_PARAMS;  
 }
 
-// returns string with the name 0f parameter n
+///////////////////////////////////////////////////////////////////////////////////////
+// getParameterName
+//
+// returns pointer to 16 byte char array containing the name of parameter specified by index
+//
+// parameters:
+// DWORD index - index of parameter 
+//
+// return values (32-bit pointer to char):
+// 32-bit pointer to array of char
+// FF_FAIL on error
+//
+
 char* getParameterName(DWORD index)
 {
 	return parameters[index].name;
 }
 
-// returns value  of parameter n
+
+///////////////////////////////////////////////////////////////////////////////////////
+// getParameterDefault
+//
+// returns default value of parameter specified by index as 32-bit float 0<=value<=1
+//
+// parameters:
+// DWORD index - index of parameter 
+//
+// return values (32-bit float):
+// 32-bit float value
+// FF_FAIL on error
+//
+
 float getParameterDefault(DWORD index)
 {
 	return parameters[index].defaultValue;
 }
 
-// returns string containing value of parameter to display
+///////////////////////////////////////////////////////////////////////////////////////
+// getParameterDisplay
+//
+// returns pointer to array of 16 char containing a string to display as the value of
+// parameter index
+//
+// parameters:
+// DWORD index - index of parameter 
+//
+// return values (32-bit pointer to char):
+// 32-bit pointer to array of char
+// FF_FAIL on error
+//
+
 char* getParameterDisplay(DWORD index)
 {
-
+	// fill the array with spaces first
+	for (int n=0; n<16; n++) {
+		parameters[index].displayValue[n] = ' ';
+	}
 	sprintf(parameters[index].displayValue, "%f",parameters[index].value);
 	return parameters[index].displayValue;
 }
 
-// returns success/fail - sets the value of parameter n in response to user
-DWORD setParameter(DWORD index, float value)
+///////////////////////////////////////////////////////////////////////////////////////
+// setParameter
+//
+// Sets the value of parameter specified by index
+// value is a 32-bit float 0<=value<=1
+//
+// parameters:
+// DWORD index - index of parameter 
+// 32-bit float value
+//
+// return values (DWORD):
+// FF_FAIL on error
+//
+
+DWORD setParameter(SetParameterStruct* pParam)
 {
-	parameters[index].value = value;
-	return 0;
+	parameters[pParam->index].value = pParam->value;
+	return FF_SUCCESS;
 }
+
+///////////////////////////////////////////////////////////////////////////////////////
+// getParameter
+//
+// returns value of parameter specified by index as 32-bit float 0<=value<=1
+//
+// parameters:
+// DWORD index - index of parameter 
+//
+// return values (32-bit float):
+// 32-bit float value
+// FF_FAIL on error
+//
 
 float getParameter(DWORD index)
 {
 	return parameters[index].value;
 }
 
-// process frame of video, returns success/fail
+///////////////////////////////////////////////////////////////////////////////////////
+// processFrame
+//
+// process a frame of video
+//
+// parameters:
+// 32-bit pointer to byte array containing frame of video
+//
+// return values (DWORD):
+// FF_SUCCESS
+// FF_FAIL on error
+//
+
 DWORD processFrame(LPVOID pFrame)
 {
-	return 999;
+	VideoPixel24bit* pPixel = (VideoPixel24bit*) pFrame;
+	for (DWORD x = 0; x < videoInfo.frameWidth; x++) {
+		for (DWORD y = 0; y < videoInfo.frameHeight; y++) {
+			pPixel->blue = (BYTE) (pPixel->blue * parameters[0].value);
+			pPixel->green = (BYTE) (pPixel->green * parameters[0].value);
+			pPixel->red = (BYTE) (pPixel->red * parameters[0].value);
+			pPixel++;
+		}
+	}
+
+	return FF_SUCCESS;
 }
 
-// returns true or false to indicate capabilities
-DWORD getPluginCaps(DWORD cap)
+///////////////////////////////////////////////////////////////////////////////////////
+// getpluginCaps
+//
+// returns true or false to indicate whether cappable of feature specified by index
+//
+// parameters:
+// DWORD index - index of parameter 
+// allowed values:
+// 0 - 16 bit video
+// 1 - 24 bit video
+// 2 - 32 bit video
+//
+// return values (DWORD):
+// FF_TRUE
+// FF_FALSE
+//
+
+DWORD getPluginCaps(DWORD index)
 {
-	return 0;
+	switch (index) {
+	case 0:
+		return FF_FALSE;
+	case 1:
+		return FF_TRUE;
+	case 2:
+		return FF_FALSE;
+	default:
+		return FF_FALSE;
+	}
 }
--- MSVCPlugin/FreeFrame.h
+++ MSVCPlugin/FreeFrame.h
@@ -1,10 +1,37 @@
-//////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
 // FreeFrame.h
+//
+// FreeFrame Open Video Plugin Prototype
+// ANSI C Version
+
+// www.freeframe.org
+// marcus@freeframe.org
+
+/*
+
+Copyright (c) 2002, Marcus Clements www.freeframe.org
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in
+     the documentation and/or other materials provided with the
+     distribution.
+   * Neither the name of FreeFrame nor the names of its
+     contributors may be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
 
 #ifndef __FREEFRAME_H__
 #define __FREEFRAME_H__
 
-//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
 //
 // includes
 //
@@ -28,11 +55,14 @@ typedef char BYTE;
 
 
 
-//////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 // #defines
 
 #define FF_EFFECT				0
 #define FF_FAIL					0xFFFFFFFF
+#define FF_SUCCESS				0
+#define FF_TRUE					1
+#define FF_FALSE				0
 
 #define FF_GETINFO				0
 #define FF_INITIALISE			1
@@ -46,6 +76,9 @@ typedef char BYTE;
 #define FF_GETPARAMETER			9
 #define FF_GETPLUGINCAPS		10
 
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+// FreeFrame types
 
 struct PlugInfoStruct {
 	DWORD	APIMajorVersion;
@@ -55,16 +88,36 @@ struct PlugInfoStruct {
 	DWORD	pluginType;				// Effect or source
 };
 
-struct ParameterStruct {
-	float value;
-	float defaultValue;
-	char displayValue[16];
-	char name[16];
+struct VideoInfoStruct {
+	DWORD frameWidth;				// width of frame in pixels
+	DWORD frameHeight;				// height of frame in pixels
+	DWORD bitDepth;					// enumerated indicator of bit depth of video 
+									// 0 = 16 bit 5-6-5   1 = 24bit packed   2 = 32bit
 };
 
+struct SetParameterStruct {
+	DWORD index;
+	float value;
+};
 
-//////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
 // Function prototypes
+//
+
+///////////////////////////////////////////////////////////////////////////////////////
+// plugMain - The one and only exposed function
+// parameters: 
+//	functionCode - tells the plugin which function is being called
+//  pParam - 32-bit parameter or 32-bit pointer to parameter structure
+//
+// PLUGIN DEVELOPERS:  you shouldn't need to change this function
+//
+// All parameters are cast as 32-bit untyped pointers and cast to appropriate
+// types here
+// 
+// All return values are cast to 32-bit untyped pointers here before return to 
+// the host
+//
 
 #ifdef WIN32
 
@@ -76,20 +129,181 @@ __declspec(dllexport) LPVOID __stdcall plugMain(DWORD functionCode, LPVOID pPara
 
 #elif LINUX
 
-  LPVOID plugMain(DWORD functionCode,LPVOID pParam);
+LPVOID plugMain(DWORD functionCode,LPVOID pParam);
 
 #endif
 
-PlugInfoStruct*	getInfo();							// get information about plugin
-DWORD	initialise();								// allocate memory, set defaults
-DWORD	deinitialise();								// deallocate memory, tidy up
-DWORD	getNumParameters();							// returns number of parameters
-char*	getParameterName(DWORD index);				// returns string with the name if parameter n
-float	getParameterDefault(DWORD index);			// returns value  of parameter n
-char*	getParameterDisplay(DWORD index);			// returns string containing value of parameter to display
-DWORD	setParameter(DWORD index, float value);		// returns success/fail - sets the value of parameter n in response to user
-float	getParameter(DWORD index);					// returns value of parameter n
-DWORD	processFrame(LPVOID pFrame);				// process frame of video, returns success/fail
-DWORD	getPluginCaps(DWORD cap);					// plugin capabilities returns true/false
+///////////////////////////////////////////////////////////////////////////////////////
+// getInfo
+//
+// gets information about the plugin - version, unique id, short name and type 
+// This function should be identical in all future versions of the FreeFrame API
+//  
+// return values (32-bit pointer to PlugInfoStruct)
+// FF_FAIL on error
+// 32-bit pointer to PlugInfoStruct
+//
+// HOST: call this function first to get version information
+//       the version defines the other fucntion codes that are supported
+//       supported function codes are listed in the documentation www.freeframe.org
+
+PlugInfoStruct*	getInfo();							
+
+///////////////////////////////////////////////////////////////////////////////////////
+// initialise
+//
+// Prepare the Plug-in for processing.  
+// Set default values, allocate memory
+// When the plug-in returns from this function it must be ready to proces a frame
+//
+// return values (DWORD)
+// FF_SUCCESS - success
+// non-zero - fail (error values to be defined)
+//
+// HOST:	This function *must* return before a call to processFrame.  Pointer to 
+//			videoInfoStruct *must* be valid until function returns
+// PLUGIN:  make a copy of the videoInfoStruct locally as pointer may not be valid after
+//			function returns
+
+DWORD	initialise(VideoInfoStruct* videoInfo);								
+
+///////////////////////////////////////////////////////////////////////////////////////
+// deinitialise
+//
+// Tidy up   
+// Deallocate memory
+//
+// return values (DWORD)
+// FF_SUCCESS - success
+// non-zero - fail (error values to be defined)
+//
+// HOST: This *must* be the last function called on the plugin
+
+DWORD	deInitialise();								
+
+///////////////////////////////////////////////////////////////////////////////////////
+// getNumParameters 
+//
+// returns number of parameters in plugin
+// 
+// return values (DWORD)
+// number of parameters
+// FF_FAIL on error
+//
+
+DWORD	getNumParameters();							
+///////////////////////////////////////////////////////////////////////////////////////
+// getParameterName
+//
+// returns pointer to 16 byte char array containing the name of parameter specified by index
+//
+// parameters:
+// DWORD index - index of parameter 
+//
+// return values (32-bit pointer to char):
+// 32-bit pointer to array of char
+// FF_FAIL on error
+//
+
+char*	getParameterName(DWORD index);				
+
+///////////////////////////////////////////////////////////////////////////////////////
+// getParameterDefault
+//
+// returns default value of parameter specified by index as 32-bit float 0<=value<=1
+//
+// parameters:
+// DWORD index - index of parameter 
+//
+// return values (32-bit float):
+// 32-bit float value
+// FF_FAIL on error
+//
+
+float	getParameterDefault(DWORD index);			
+
+///////////////////////////////////////////////////////////////////////////////////////
+// getParameterDisplay
+//
+// returns pointer to array of 16 char containing a string to display as the value of
+// parameter index
+//
+// parameters:
+// DWORD index - index of parameter 
+//
+// return values (32-bit pointer to char):
+// 32-bit pointer to array of char
+// FF_FAIL on error
+//
+
+char*	getParameterDisplay(DWORD index);			
+
+///////////////////////////////////////////////////////////////////////////////////////
+// setParameter
+//
+// Sets the value of parameter specified by index
+// value is a 32-bit float 0<=value<=1
+//
+// parameters:
+// DWORD index - index of parameter 
+// 32-bit float value
+//
+// return values (DWORD):
+// FF_FAIL on error
+//
+
+DWORD	setParameter(SetParameterStruct* pParam);		
+
+///////////////////////////////////////////////////////////////////////////////////////
+// getParameter
+//
+// returns value of parameter specified by index as 32-bit float 0<=value<=1
+//
+// parameters:
+// DWORD index - index of parameter 
+//
+// return values (32-bit float):
+// 32-bit float value
+// FF_FAIL on error
+//
+
+float	getParameter(DWORD index);					
+
+///////////////////////////////////////////////////////////////////////////////////////
+// processFrame
+//
+// process a frame of video 'in place' 
+//
+// parameters:
+// 32-bit pointer to byte array containing frame of video
+//
+// return values (DWORD):
+// FF_SUCCESS
+// FF_FAIL on error
+//
+// HOST: pFrame needs to be a valid pointer throughout this call as the plugin processes
+// the frame 'in place'.
+
+DWORD	processFrame(LPVOID pFrame);				
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+// getpluginCaps
+//
+// returns true pof false to indicate whether cappable of feature specified by index
+//
+// parameters:
+// DWORD index - index of parameter 
+// allowed values:
+// 0 - 16 bit video
+// 1 - 24 bit video
+// 2 - 32 bit video
+//
+// return values (DWORD):
+// FF_TRUE
+// FF_FALSE
+//
+
+DWORD	getPluginCaps(DWORD index);					
 
 #endif
